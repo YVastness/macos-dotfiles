ab391fc754f438822f74b026896ff1003f7770ed
Vastness <54315142+YVastness@users.noreply.github.com>
2025-08-08 21:53:58 +0800

    update

--- dot_config/init/macos_init.sh
+++ dot_config/init/macos_init.sh
@@ -1,2 +1,9 @@
+<<<<<<< HEAD
 brew install wechat douyin notion google-chrome bilibili termius warp qbittorrent tencent-lemon IINA neovim lazygit lazyvim wpsoffice-cn free-download-manager monitorcontrol visual-studio-code raycast chatgpt capcut baidunetdisk adrive qq canva wetype youku claude folo chezmoi git superfile exiftool
+=======
+#!/usr/bin/env bash
+# author:yinhaoyu
+
+brew install wechat douyin notion google-chrome bilibili termius warp qbittorrent tencent-lemon IINA neovim lazygit lazyvim wpsoffice-cn free-download-manager monitorcontrol visual-studio-code raycast chatgpt capcut baidunetdisk adrive qq canva wetype youku claude folo chezmoi git zoxide superfile exiftool
+>>>>>>> 96eadd0 (update)
 brew install yazi ffmpeg sevenzip jq poppler fd ripgrep fzf zoxide resvg imagemagick font-symbols-only-nerd-font # yazi 及 扩展
