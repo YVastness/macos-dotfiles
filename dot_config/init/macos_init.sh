2cdf156114876d4378f013c565bd54f210358693
yinhaoyu <1916286435@qq.com>
2026-03-07 20:00:40 +0800

    update

--- dot_config/init/macos_init.sh
+++ dot_config/init/macos_init.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 # author:yinhaoyu
 
-brew install wechat douyin notion google-chrome bilibili termius warp qbittorrent tencent-lemon IINA neovim lazygit lazyvim wpsoffice-cn free-download-manager monitorcontrol visual-studio-code raycast chatgpt capcut baidunetdisk adrive qq canva wetype youku claude folo chezmoi git zoxide superfile exiftool lazyssh git-delta
+brew install neovim lazygit lazyvim chezmoi git zoxide superfile exiftool lazyssh git-delta
+brew install --cask wechat douyin notion google-chrome bilibili termius warp qbittorrent tencent-lemon IINA wpsoffice-cn free-download-manager monitorcontrol visual-studio-code raycast chatgpt baidunetdisk adrive qq canva wetype youku claude folo stash doubao feishu flykey qianwen tencent-meeting qqlive docker-desktop github input-source-pro keka kindavim Nugget Obsidian snipaste telegram UTM Zed
 brew install yazi ffmpeg sevenzip jq poppler fd ripgrep fzf zoxide resvg imagemagick font-symbols-only-nerd-font # yazi 及 扩展
